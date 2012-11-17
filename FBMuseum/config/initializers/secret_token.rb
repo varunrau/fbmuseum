c74c283011f6c95afbc1a73bd09e0f097f174a84
varunrau <varunrau@gmail.com>
2012-11-17 12:05:12 -0800

    created the rails app

---
+++ FBMuseum/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FBMuseum::Application.config.secret_token = 'b763e62c8aca8e324566ae99a14eac458fbee086c3acf0ccfe19dc128a2ebafc62b4cba1d6021f3595919cf4a28473ac4b87245a8f40f320b81f517a83954e8e'
